0aafb7619f10a795386c73bed7940328d7d28346
yoGhastly <ve06976@innovaccion.mx>
2023-10-05 18:49:29 -0600

    footer redirects to xervsware

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 export const Footer = () => {
   return (
     <footer className="flex flex-nowrap md:flex-wrap h-24 w-full items-center justify-center border-t">
-      <p className='text-primary text-center'>© 2023 Derechos Reservados. Elaborado por <span className='font-bold'>Xervsware</span></p>
+      <p className="text-primary text-center">
+        © 2023 Derechos Reservados. Elaborado por{" "}
+        <Link href="https://www.instagram.com/xervsware/" className="font-bold">
+          Xervsware
+        </Link>
+      </p>
     </footer>
-  )
-}
+  );
+};
